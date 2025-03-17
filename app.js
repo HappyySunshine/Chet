84e89c9b1a9a8299d94ab5961a5fabd017cb5768
Nicolas Hugo Ferreira Ouellet - 13525119932 <13525119932@ulife.com.br>
2025-03-17 11:28:11 -0300

    m

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const path = require('path');
+const cors = require('cors')
 const app = express()
 const port = 3000
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cors());
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 })
--- public/index.js
+++ public/index.js
@@ -1,1 +1,15 @@
-console.log("hiii")
+const app = angular.module("MyApp", []);
+
+app.
+  component('inputWidget', {
+    template: 
+    '<div class="field-widget">' +
+                '<p class = "field-widget-text">{{$ctrl.text}}</p>'+
+               '<input class="field-widget-input" type="{{$ctrl.type}}" id="{{$ctrl.id}}" name="fname">' +
+            '</div>',
+    controller: function inputWidget() {
+      this.text = "login :"
+      this.type = "text"
+      this.id= "username"
+    }
+  });
